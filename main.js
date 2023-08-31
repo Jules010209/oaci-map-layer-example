eadb08545007df843d9073eeda29756d76c307ce
Jules Chenelat <67187964+Jules010209@users.noreply.github.com>
2023-08-31 18:33:22 +0200

    add: weather layer

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ let oaciLayer = L.tileLayer('https://sdoaci.skydreamsoft.fr/current_cycle/Z{z}/{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+const getNewestLayer = async() => {
+    return await fetch('https://api.rainviewer.com/public/maps.json')
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error();
+            }
+
+            return response.json();
+        })
+        .then(timestamps => timestamps.sort())
+        .then(timestamps => {
+            return L.tileLayer(`https://tilecache.rainviewer.com/v2/radar/${timestamps[timestamps.length - 1]}/256/{z}/{x}/{y}/2/1_1.png`, {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+        })
+        .catch(err => console.error(err));
+}
+
+getNewestLayer();
+
+setTimeout(getNewestLayer(), 10000);
+
 var layers = L.layerGroup([mainLayer, oaciLayer]);
 
 var map = L.map('map', {
